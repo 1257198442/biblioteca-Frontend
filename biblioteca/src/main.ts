e1226ebb5db376e4b09784f65d86ef67168d29ec
JiamingSHI <1257198442@qq.com>
2024-02-11 01:00:46 +0100

    Inicialización del proyecto

---
+++ biblioteca/src/main.ts
@@ -0,0 +1,12 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+console.log("  _     ___ ____  ____      _    ______   __\n" +
+  " | |   |_ _| __ )|  _ \\    / \\  |  _ \\ \\ / /\n" +
+  " | |    | ||  _ \\| |_) |  / _ \\ | |_) \\ V / \n" +
+  " | |___ | || |_) |  _ <  / ___ \\|  _ < | |  \n" +
+  " |_____|___|____/|_| \\_\\/_/   \\_\\_| \\_\\|_|  ")
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
