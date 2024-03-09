ba5765e7e7d21ff1caeba0c0b765872ed086f1e9
JiamingSHI <1257198442@qq.com>
2024-03-09 03:30:57 +0100

    Implementar la interfaz y funcionalidad de la página principal del usuario

--- biblioteca/src/app/endPoints.ts
+++ biblioteca/src/app/endPoints.ts
@@ -1,4 +1,5 @@
 export class endPoints {
   static user:string = "https://localhost/user";
   static library : string = "https://localhost/library";
+  static wallet : string = "https://localhost/wallet";
 }
